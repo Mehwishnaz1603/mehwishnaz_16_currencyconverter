6e8d5997d78621d4bf7e4c84c2a737aaf556bdfe
Mehwishnaz <160154061+Mehwishnaz1603@users.noreply.github.com>
2024-04-06 16:51:28 +0500

    finallu updated

--- index.ts
+++ index.ts
@@ -1,27 +1,23 @@
-import inquirer from "inquirer";
-import chalk from "chalk";
-import { Console } from "console";
-const currency: any = {
-    USD: 1,       PKR: 277.54,  EURO: 0.92,  
-    RUB: 92.58,   SAR: 3.75,    INR: 83.30,
-    GBP: 0.79,    AUD: 1.52,    TRY: 32.03,   JPY: 151.59,
-}
+import inquirer from "inquirer";   import chalk from "chalk";
+const currency: any = {USD: 1,  PKR: 277.54,  EURO: 0.92,  
+    RUB: 92.58,   SAR: 3.75,    INR: 83.30,  GBP: 0.79,    AUD: 1.52,    
+    TRY: 32.03,   JPY: 151.59,}
 let user_answer = await inquirer.prompt([
-        {
-            name: "FromCurrency",  type: "list",
-            message: chalk.bold.underline("Select Your Currency: "),
-            choices: ["USD","PKR","EURO","RUB","SAR","INR",
+     {
+         name: "FromCurrency",  type: "list",
+         message: chalk.bold.underline("Select Your Currency: "),
+         choices: ["USD","PKR","EURO","RUB","SAR","INR",
                        "GBP","AUD","TRY","JPY"]
         },
         {
-            name: "ToCurrency",   type:  "list",
-            message: chalk.bold.underline("Enter to currency: "),
-            choices: ["USD","PKR","EURO","RUB","SAR","INR",
+         name: "ToCurrency",   type:  "list",
+         message: chalk.bold.underline("Enter to currency: "),
+         choices: ["USD","PKR","EURO","RUB","SAR","INR",
                        "GBP","AUD","TRY","JPY"]
         },
         {
-            name: "Amount",   type: "number",
-            message:chalk.bold.underline ("Enter Your Amount You want to Convert:"),
+         name: "Amount",   type: "number",
+         message:chalk.bold.underline ("Enter Your Amount You want to Convert:"),
         }
     ]); 
     if (user_answer.FromCurrency === user_answer.ToCurrency)
